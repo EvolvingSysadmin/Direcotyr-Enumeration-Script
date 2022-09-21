4a6039471d454e221dc3e8f1a6848c1e56a4f361
condor <lebowski.lab@gmail.com>
2022-09-21 10:48:36 -0700

    added script & updated readme

---
+++ Directory-Enumeration.ps1
@@ -0,0 +1,12 @@
+﻿# Get variables for directory path and report path
+
+$DirectoryLocation = Read-Host -Prompt "Input the full path of the directory being enumerated without quotes"
+$OutputFileLocation = Read-Host -Prompt "Input the full path where the txt file should be saved without quotes"
+
+# Enumerate directory files and write to report
+
+Get-ChildItem -Path $DirectoryLocation -Recurse -Force | Out-File -FilePath $OutputFileLocation
+
+# Get count of files and sum of file sizes in bytes and append to report
+
+Get-ChildItem -Path $DirectoryLocation | Measure-Object -Sum Length | Select-Object Count, Sum | Out-File -FilePath $OutputFileLocation -Append
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Direcotyr-Enumeration-Script
 Simple PowerShell script that enumerates, counts, and totals files in a directory and prints them to a txt report.
+
+## Usage
+
+1. Run the PowerShell script
+2. Specify the directory to be enumerated when prompted
+3. Specify the txt report path when prompted
+4. Txt report will list files (including subdirectories), file sizes, last modified dates, total file size and file count
